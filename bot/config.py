d22db2cf0272a6303643f4b8af7acb5d71b9fb49
yunmemg <13362553093@163.com>
2026-08-18 00:52:38 +0800

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
+    ALLOWED_USER_IDS: list[int] = [
+        int(x.strip())
+        for x in os.getenv("ALLOWED_USER_IDS", "").split(",")
+        if x.strip().isdigit()
+    ]
+    ADMIN_IDS: list[int] = [
+        int(x.strip())
+        for x in os.getenv("ADMIN_IDS", "").split(",")
+        if x.strip().isdigit()
+    ]
+    DB_PATH: str = os.getenv("DB_PATH", "")
+
+
+config = Config()
